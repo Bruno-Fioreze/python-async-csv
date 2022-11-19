6bdf9de986e6d50f627fc4d11693807b41dc9b38
Bruno Fioreze <brunofioreze@pop-os.localdomain>
2022-11-19 13:47:00 -0300

    import decorator limit concurrency

--- decorator.py
+++ decorator.py
@@ -15,14 +15,14 @@ def request_concurrency_limit_decorator(limit=3):
 
     return executor
 
-@request_concurrency_limit_decorator(limit=3)
-async def download():
-    await asyncio.sleep(3)
-    print("chegou aqui")
-    ...
+# @request_concurrency_limit_decorator(limit=3)
+# async def download():
+#     await asyncio.sleep(3)
+#     print("chegou aqui")
+#     ...
 
-async def coro():
-    await asyncio.gather( *[ download() for number in range(0, 10)])
+# async def coro():
+#     await asyncio.gather( *[ download() for number in range(0, 10)])
 
-asyncio.run(coro())
+# asyncio.run(coro())
 
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import asyncio
 import aiofiles
 from aiocsv import AsyncReader, AsyncDictReader, AsyncWriter, AsyncDictWriter
+from decorator import request_concurrency_limit_decorator
 
 
+# @request_concurrency_limit_decorator(limit=1)
 async def read_file(file: str):
-    print("========== start ============")
     async with aiofiles.open(file, mode="r", encoding="utf-8", newline="") as afp:
         async for row in AsyncDictReader(afp):
             print(row)  # row is a list
-    print("========== finish ============")
 
 async def coro():
     await asyncio.gather(read_file("./addresses.csv"), read_file("./addresses2.csv"))
@@ -16,23 +16,3 @@ async def coro():
 
 asyncio.run(coro())
 
-
-import asyncio
-from functools import wraps
-def request_concurrency_limit_decorator(limit=3):
-    # Bind the default event loop 
-    sem = asyncio.Semaphore(limit)
-
-    def executor(func):
-        @wraps(func)
-        async def wrapper(*args, **kwargs):
-            async with sem:
-                return await func(*args, **kwargs)
-
-        return wrapper
-
-    return executor
-
-@request_concurrency_limit_decorator(limit=...)
-async def download():
-    ...
